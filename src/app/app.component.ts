9477c23638a5bcff8bd689e44d6aea1232fd66d9
Hugo <hugoblanc.blend@gmail.com>
2019-07-01 19:52:05 +0200

    FEAT - Add firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FirebaseLib } from '@ionic-native/firebase-lib';
 
 @Component({
   selector: 'app-root',
@@ -37,7 +38,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private firebaseLib: FirebaseLib
   ) {
     this.initializeApp();
   }
