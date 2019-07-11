e4d5e9ce28735bf7532be6e39c48645dc40a9a46
Hugo <hugoblanc.blend@gmail.com>
2019-07-11 14:34:23 +0200

    Création Header composant

--- src/app/components/ath-header/ath-header.component.ts
+++ src/app/components/ath-header/ath-header.component.ts
@@ -1,14 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input, ElementRef, Renderer2 } from '@angular/core';
+import { StyleService } from '../../provider/style.service';
 
 @Component({
-  selector: 'ath-ath-header',
+  selector: 'ath-header',
   templateUrl: './ath-header.component.html',
   styleUrls: ['./ath-header.component.scss'],
 })
-export class AthHeaderComponent implements OnInit {
+export class AthHeaderComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @Input() title?: string;
+  @Input() help?: string;
+  @Input() callOption?: Function;
+  @Input() scrollArea?: any;
+  // tslint:disable-next-line: variable-name
+  private _pureHeader: boolean; // Seulement les boutton ou pas
 
-  ngOnInit() {}
 
+  @Input()
+  set pureHeader(val: boolean) {
+    this._pureHeader = val;
+    this.dealWithPureHeader();
+  }
+
+
+
+
+  newHeaderHeight: any;
+  dynamic = '';
+  shadowIsDisplayed = false;
+  classOverride = '';
+  constructor(public style: StyleService,
+              public element: ElementRef,
+              public renderer: Renderer2) {
+    console.log(element);
+  }
+
+  ngOnInit() {
+    this.dealWithPureHeader();
+  }
+
+
+
+
+  ngAfterViewInit() {
+    if (this.scrollArea != null && this._pureHeader !== true) {
+      this.scrollArea.ionScroll.subscribe((ev) => {
+        this.resizeHeader(ev);
+      });
+    } else if (this.scrollArea === undefined && this._pureHeader === undefined) {
+      this.renderer.addClass(this.element.nativeElement, 'image-is-missing');
+      this.renderer.removeClass(this.element.nativeElement, 'image-is-present');
+    }
+  }
+
+  resizeHeader(ev) {
+
+
+    const shadows = document.querySelectorAll('#shad');
+    if (this.shadowIsDisplayed === false && ev.detail.scrollTop > 67) {
+      this.renderer.setStyle(shadows[shadows.length - 1], 'height', '5%');
+      this.shadowIsDisplayed = true;
+    } else if (this.shadowIsDisplayed === true && ev.detail.scrollTop <= 67) {
+      this.shadowIsDisplayed = false;
+      this.renderer.setStyle(shadows[shadows.length - 1], 'height', '0%');
+    }
+
+
+  }
+
+
+  private dealWithPureHeader() {
+    if (this._pureHeader === true) {
+      const shadows = document.querySelectorAll('#ath-header');
+      this.renderer.addClass(shadows[shadows.length - 1], 'pure-ath-header');
+      this.renderer.addClass(this.element.nativeElement, 'image-is-present');
+      this.renderer.removeClass(this.element.nativeElement, 'image-is-missing');
+      this.classOverride = 'pureHeader';
+    } else if (this._pureHeader === false) {
+      this.renderer.addClass(this.element.nativeElement, 'image-is-missing');
+      this.renderer.removeClass(this.element.nativeElement, 'image-is-present');
+    }
+  }
 }
+
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AthLoaderComponent } from './ath-loader/ath-loader.component';
 import { IonicModule } from '@ionic/angular';
+import { AthHeaderComponent } from './ath-header/ath-header.component';
 
 @NgModule({
-  declarations: [AthLoaderComponent],
-  exports: [AthLoaderComponent],
+  declarations: [AthLoaderComponent, AthHeaderComponent],
+  exports: [AthLoaderComponent, AthHeaderComponent],
   imports: [
     CommonModule,
     IonicModule.forRoot(),
