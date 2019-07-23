4214834a46eb0c8a15d9f07005c8024954308d88
Hugo <hugoblanc.blend@gmail.com>
2019-07-23 16:29:45 +0200

    FIX - IOS color and size

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AthLoaderComponent } from './ath-loader/ath-loader.component';
 import { IonicModule } from '@ionic/angular';
-import { AthHeaderComponent } from './ath-header/ath-header.component';
 
 @NgModule({
-  declarations: [AthLoaderComponent, AthHeaderComponent],
-  exports: [AthLoaderComponent, AthHeaderComponent],
+  declarations: [AthLoaderComponent],
+  exports: [AthLoaderComponent],
   imports: [
     CommonModule,
     IonicModule.forRoot(),
