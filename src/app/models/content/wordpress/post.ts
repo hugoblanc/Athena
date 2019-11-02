d36aa89c9fffc025e5317dedb4f6c948689f58f2
MrWhite <hugoblanc.blend@gmail.com>
2019-11-02 17:58:35 +0100

    fix: use real image url

--- src/app/models/content/wordpress/post.ts
+++ src/app/models/content/wordpress/post.ts
@@ -56,12 +56,12 @@ export class Post implements IContent {
       if (this.guid &&
         this.guid.rendered &&
         this.embedded &&
+        this.embedded.featuredmedia &&
         this.embedded.featuredmedia[0] &&
-        this.embedded.featuredmedia[0].mediaDetails &&
-        this.embedded.featuredmedia[0].mediaDetails.file) {
-        const url = this.guid.rendered.split('?');
+        this.embedded.featuredmedia[0].mediaDetails) {
+
         this.image = {
-          url: url[0] + 'wp-content/uploads/' + this.embedded.featuredmedia[0].mediaDetails.file,
+          url: this.embedded.featuredmedia[0].sourceUrl,
           height: this.embedded.featuredmedia[0].mediaDetails.height,
           width: this.embedded.featuredmedia[0].mediaDetails.width
         };
