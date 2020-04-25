53665a940f0bb49a60785a3cc201889ce4ea384f
MrWhite <hugoblanc.blend@gmail.com>
2020-04-25 23:04:39 +0200

    refactor: app routing import esnext

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,28 @@ import { CheckFirstGuard } from './core/check-first.guard';
 import { CurrentMetaMediaGuard } from './core/current-meta-media.guard';
 
 const routes: Routes = [
-  // { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'media/:key', canActivate: [CurrentMetaMediaGuard], loadChildren: './media/media.module#MediaPageModule' },
-  // tslint:disable-next-line: max-line-length
-  { path: 'media/:key/details/:id', canActivate: [CurrentMetaMediaGuard], loadChildren: './content-details/content-details.module#ContentDetailsPageModule' },
-  { path: 'informations', loadChildren: './informations/informations.module#InformationsPageModule' },
-  {
-    path: 'tuto',
-    loadChildren: () => import('./pages/tuto/tuto.module').then(m => m.TutoPageModule)
-  },
   {
     path: '',
     loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule),
     canLoad: [CheckFirstGuard]
+  },
+  {
+    path: 'media/:key',
+    canActivate: [CurrentMetaMediaGuard],
+    loadChildren: () => import('./media/media.module').then(m => m.MediaPageModule)
+  },
+  {
+    path: 'media/:key/details/:id',
+    canActivate: [CurrentMetaMediaGuard],
+    loadChildren: () => import('./content-details/content-details.module').then(m => m.ContentDetailsPageModule)
+  },
+  {
+    path: 'informations',
+    loadChildren: () => import('./pages/informations/informations.module').then(m => m.InformationsPageModule)
+  },
+  {
+    path: 'tuto',
+    loadChildren: () => import('./pages/tuto/tuto.module').then(m => m.TutoPageModule)
   },
   {
     path: 'construction',
