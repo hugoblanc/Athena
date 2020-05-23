74e3d112c0e42e0f2f8f22ddaca647e30822b955
MrWhite <hugoblanc.blend@gmail.com>
2020-05-23 18:02:39 +0200

    test: enforce alert construction check

--- src/app/provider/helper/help.service.spec.ts
+++ src/app/provider/helper/help.service.spec.ts
@@ -3,6 +3,9 @@ import { TestBed } from '@angular/core/testing';
 import { HelpService } from './help.service';
 
 describe('HelpService', () => {
+  const validCode = 'content-details';
+  const invalidCode = 'AZERTUIOPIUYREZA';
+
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
@@ -10,10 +13,40 @@ describe('HelpService', () => {
     expect(service).toBeTruthy();
   });
 
+
+  it('should contains a findHelpFromCode method', () => {
+    const service: HelpService = TestBed.get(HelpService);
+    expect(service.findHelpFromCode).toBeTruthy();
+  });
+
+
+  it('should contains a findHelpFromCode method which return Help', () => {
+    const service: HelpService = TestBed.get(HelpService);
+    const help = service.findHelpFromCode(validCode);
+    expect(help).toBeTruthy();
+  });
+
+
+  it('should contains a findHelpFromCode method which return Help', () => {
+    const service: HelpService = TestBed.get(HelpService);
+    expect(service.findHelpFromCode(invalidCode)).toThrowError();
+  });
+
+
   it('should contains a displayHelp method', () => {
     const service: HelpService = TestBed.get(HelpService);
     expect(service.displayHelp).toBeTruthy();
   });
 
+
+  it('should contains a displayHelp method which construct corresponding alert', async () => {
+    const service: HelpService = TestBed.get(HelpService);
+
+    const help = service.findHelpFromCode(validCode);
+    const alert = await service.displayHelp(validCode);
+
+    expect(alert.title).toEqual(help.title);
+    expect(alert.textContent).toEqual(help.text);
+  });
 
 });
