58d5963ba5633f112806366828ca44a71b0989ba
MrWhite <hugoblanc.blend@gmail.com>
2020-05-24 19:36:07 +0200

    test: guards ok

--- src/app/core/check-first.guard.spec.ts
+++ src/app/core/check-first.guard.spec.ts
@@ -1,13 +1,15 @@
 import { inject, TestBed } from '@angular/core/testing';
 import { CheckFirstGuard } from './check-first.guard';
-import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { Storage } from '@ionic/storage';
+import { storageIonicMock } from '../../testing/storage-mock';
 
 
 describe('CheckFirstGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CheckFirstGuard, Storage, Router]
+      providers: [CheckFirstGuard, { provide: Storage, useValue: storageIonicMock }],
+      imports: [RouterTestingModule]
     });
   });
 
