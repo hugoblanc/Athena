baee9f01333e2b74e732d23c9e4b78dfdc0ef0e7
MrWhite <hugoblanc.blend@gmail.com>
2020-05-31 17:38:07 +0200

    test: restore content-details-page test case

--- src/app/content-details/content-details.page.spec.ts
+++ src/app/content-details/content-details.page.spec.ts
@@ -4,21 +4,34 @@ import { ContentDetailsPage } from './content-details.page';
 import { postMock } from '../../testing/postMock';
 import { ContentService } from '../provider/content/content.service';
 import { of } from 'rxjs';
+import { MetaMediaService } from '../provider/meta-media/meta-media.service';
+import { MetaMediaType } from '../models/meta-media/meta-media-type.enum';
+import { RouterTestingModule } from '@angular/router/testing';
 
 
 describe('ContentDetailsPage', () => {
   let component: ContentDetailsPage;
   let fixture: ComponentFixture<ContentDetailsPage>;
 
-  const contentServiceSpy = jasmine.createSpyObj('ContentService', {getContentById:  of(postMock)});
+  const contentServiceSpy = jasmine.createSpyObj('ContentService', { getContentById: of(postMock) });
+
+  const metaMediaSpyMethod = jasmine.createSpyObj('MetaMediaService', { getContentById: of(postMock) });
+  metaMediaSpyMethod.currentMetaMedia = { type: MetaMediaType.WORDPRESS };
 
   beforeEach(async(() => {
+
     TestBed.configureTestingModule({
-      declarations: [ ContentDetailsPage ],
+      declarations: [ContentDetailsPage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      providers: [{provide: ContentService, useValue: contentServiceSpy}]
+      imports: [RouterTestingModule]
+    }).overrideComponent(ContentDetailsPage, {
+      set: {
+        providers: [
+          {provide: ContentService, useValue: contentServiceSpy}
+        ]
+      }
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ContentDetailsPage);
     component = fixture.componentInstance;
