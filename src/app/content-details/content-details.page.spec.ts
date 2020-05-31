8c270dca6db6bd7227693b252679496d2bbaab22
MrWhite <hugoblanc.blend@gmail.com>
2020-05-31 17:51:10 +0200

    test: check init style is correct

--- src/app/content-details/content-details.page.spec.ts
+++ src/app/content-details/content-details.page.spec.ts
@@ -1,12 +1,15 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ContentDetailsPage } from './content-details.page';
-import { postMock } from '../../testing/postMock';
-import { ContentService } from '../provider/content/content.service';
-import { of } from 'rxjs';
-import { MetaMediaService } from '../provider/meta-media/meta-media.service';
-import { MetaMediaType } from '../models/meta-media/meta-media-type.enum';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { postMock } from '../../testing/postMock';
+import { MetaMediaType } from '../models/meta-media/meta-media-type.enum';
+import { ContentService } from '../provider/content/content.service';
+import { ContentDetailsPage } from './content-details.page';
+import { StyleService } from '../provider/style.service';
+import { LinkService } from '../provider/helper/link.service';
+import { Storage } from '@ionic/storage';
+import { storageIonicMock } from '../../testing/storage-mock';
 
 
 describe('ContentDetailsPage', () => {
@@ -18,11 +21,19 @@ describe('ContentDetailsPage', () => {
   const metaMediaSpyMethod = jasmine.createSpyObj('MetaMediaService', { getContentById: of(postMock) });
   metaMediaSpyMethod.currentMetaMedia = { type: MetaMediaType.WORDPRESS };
 
+  const styleServiceSpy = jasmine.createSpyObj('StyleService', { initPage: null, leavePage: null });
+  const linkServiceSpy = jasmine.createSpyObj('LinkService', { enableDynamicHyperlinks: null});
+
   beforeEach(async(() => {
 
     TestBed.configureTestingModule({
       declarations: [ContentDetailsPage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {provide: StyleService, useValue: styleServiceSpy},
+        {provide: LinkService, useValue: linkServiceSpy},
+        {provide: Storage, useValue: storageIonicMock}
+      ],
       imports: [RouterTestingModule]
     }).overrideComponent(ContentDetailsPage, {
       set: {
@@ -41,5 +52,9 @@ describe('ContentDetailsPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should restore style settings', async () => {
+    await expect(styleServiceSpy.initPage).toHaveBeenCalled();
   });
 });
