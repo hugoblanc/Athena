d3936f0b9baf32cd0a1ebbf6b9a99b72e041c59a
MrWhite <hugoblanc.blend@gmail.com>
2020-06-09 19:16:11 +0200

    test: notification service

--- src/app/provider/notification.service.spec.ts
+++ src/app/provider/notification.service.spec.ts
@@ -1,18 +1,62 @@
 import { TestBed } from '@angular/core/testing';
-import { NotificationService } from './notification.service';
+import { Router } from '@angular/router';
+import { FirebaseX } from '@ionic-native/firebase-x/ngx';
+import { MetaMedia } from '../models/meta-media/meta-media';
 import { StorageService } from './helper/storage.service';
 import { MetaMediaService } from './meta-media/meta-media.service';
-import { FirebaseX } from '@ionic-native/firebase-x/ngx';
-import { Router } from '@angular/router';
-
-
+import { NotificationService } from './notification.service';
 describe('NotificationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [StorageService, MetaMediaService, FirebaseX, Router]
-  }));
-
-  it('should be created', () => {
-    const service: NotificationService = TestBed.get(NotificationService);
+  let service: NotificationService;
+  beforeEach(() => {
+    const routerStub = () => ({ navigateByUrl: arg => ({}) });
+    const firebaseXStub = () => ({
+      getToken: () => ({ then: () => ({}) }),
+      onMessageReceived: () => ({ subscribe: f => f({}) }),
+      grantPermission: () => ({}),
+      subscribe: f => f({}),
+      unsubscribe: topic => ({})
+    });
+    const metaMediaStub = () => ({ notification: {}, key: {} });
+    const storageServiceStub = () => ({
+      get: nOTIFICATIONS_TOPICS_CATEGORIES => ({
+        subscribe: f => f({}),
+        pipe: () => ({})
+      }),
+      set: (
+        nOTIFICATIONS_TOPICS_CATEGORIES,
+        notificationTopicsCategories
+      ) => ({}),
+      editObject: (nOTIFICATIONS_TOPICS, topic, arg) => ({})
+    });
+    const metaMediaServiceStub = () => ({
+      listMetaMedia: {},
+      findAndSetMediaByKey: topic => ({ notification: {} })
+    });
+    TestBed.configureTestingModule({
+      providers: [
+        NotificationService,
+        { provide: Router, useFactory: routerStub },
+        { provide: FirebaseX, useFactory: firebaseXStub },
+        { provide: MetaMedia, useFactory: metaMediaStub },
+        { provide: StorageService, useFactory: storageServiceStub },
+        { provide: MetaMediaService, useFactory: metaMediaServiceStub }
+      ]
+    });
+    service = TestBed.get(NotificationService);
+  });
+  it('can load instance', () => {
     expect(service).toBeTruthy();
+  });
+  describe('initOpenNotification', () => {
+    it('makes expected calls', () => {
+      const routerStub: Router = TestBed.get(Router);
+      const firebaseXStub: FirebaseX = TestBed.get(FirebaseX);
+      spyOn(routerStub, 'navigateByUrl').and.callThrough();
+      spyOn(firebaseXStub, 'getToken').and.callThrough();
+      spyOn(firebaseXStub, 'onMessageReceived').and.callThrough();
+      service.initOpenNotification();
+      expect(firebaseXStub.getToken).toHaveBeenCalled();
+      expect(firebaseXStub.onMessageReceived).toHaveBeenCalled();
+    });
   });
 });
